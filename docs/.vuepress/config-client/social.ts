a9ae55f88c99c426e8826309ac49dfa055787ab2
mykhailo <m.diakovych13@gmail.com>
2023-04-25 16:10:28 +0300

    clear branch

--- docs/.vuepress/config-client/social.ts
+++
@@ -1,9 +0,0 @@
-export default [
-    {url: "https://www.facebook.com/cloudlinux/", logo: "fb.png"},
-    {url: "https://twitter.com/cloudlinuxos/", logo: "tw.png"},
-    {url: "https://linkedin.com/company/cloudlinux", logo: "in.png"},
-    {
-        url: "https://www.youtube.com/channel/UCZ3YMHWnMP7TaxlXVay5-aw",
-        logo: "ytube.svg"
-    }
-]
--- docs/.vuepress/config.ts
+++
@@ -1,9 +0,0 @@
-import {App, defineUserConfig} from "vuepress";
-
-import theme from "./theme"
-import plugins from "./config-user/plugins";
-
-export default defineUserConfig({
-    theme,
-    plugins,
-});
