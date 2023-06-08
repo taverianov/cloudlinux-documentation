edd1fd24897f55fbca045f6baf8e1bd841f17407
mykhailo <m.diakovych13@gmail.com>
2023-06-08 14:29:20 +0300

    added redirection from 404 page for full urls

--- docs/.vuepress/theme/layouts/NotFound.vue
+++ docs/.vuepress/theme/layouts/NotFound.vue
@@ -14,13 +14,18 @@ const allPages = ref([])
 
 const redirectList = [
   {
+    from: "cloudlinux-os-plus/#get-started",
+    to: "shared-pro/accelerate-wp/#get-started"
+  },
+  {
     from: "cloudlinux-os-plus",
     to: "shared-pro"
   }
 ]
 
 onMounted(()=>{
-  const regex = new RegExp(route.path + route.hash,'gi')
+  const fullPath = route.path + route.hash;
+  const regex = new RegExp(fullPath,'gi')
 
   let haveSolution = false;
 
@@ -48,8 +53,8 @@ onMounted(()=>{
     const values = Object?.values(pagesData);
 
     for (let redirection of redirectList) {
-      if (route.path.search(redirection.from) !== -1) {
-        const path = route.path.replace(redirection.from, redirection.to) + route.hash;
+      if (fullPath.search(redirection.from) !== -1) {
+        const path = fullPath.replace(redirection.from, redirection.to);
 
         router.push(path);
         return;
